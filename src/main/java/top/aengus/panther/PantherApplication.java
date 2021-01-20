b576454c496f402c7e859d4d2945519099696382
Aengus Sun <sys6511@126.com>
2021-01-20 11:35:34 +0800

    refactor: 移动Application类功能到启动类

--- src/main/java/top/aengus/panther/PantherApplication.java
+++ src/main/java/top/aengus/panther/PantherApplication.java
@@ -2,9 +2,18 @@ package top.aengus.panther;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EntityScan(basePackages = "top.aengus.panther.model")
 @SpringBootApplication
-public class PantherApplication {
+public class PantherApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PantherApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PantherApplication.class, args);
