7503f0e25e9795044dee8e827e95ba3f13b85061
Aengus Sun <sys6511@126.com>
2021-04-14 17:34:30 +0800

    feat: 新增日期格式化方法

--- src/main/java/top/aengus/panther/tool/DateFormatter.java
+++ src/main/java/top/aengus/panther/tool/DateFormatter.java
@@ -12,6 +12,7 @@ public class DateFormatter {
 
     private static final SimpleDateFormat SEPARATOR_FORMATTER = new SimpleDateFormat("yyyy/MM/dd/HH/mm/");
     private static final SimpleDateFormat HOUR_COLON_MIN_FORMATTER = new SimpleDateFormat("HH:mm");
+    private static final SimpleDateFormat DATE_UNDERLINE_FORMATTER = new SimpleDateFormat("yyyy_MM_dd");
 
     public static String separatorFormat(Date date) {
         return SEPARATOR_FORMATTER.format(date);
@@ -19,5 +20,9 @@ public class DateFormatter {
 
     public static String hourColonMinFormat(Date date) {
         return HOUR_COLON_MIN_FORMATTER.format(date);
+    }
+    
+    public static String dateUnderlineFormat(Date date) {
+        return DATE_UNDERLINE_FORMATTER.format(date);
     }
 }
