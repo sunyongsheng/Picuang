7ea1f86082b07412be85cb87a2d0f31a51561a78
Aengus Sun <sys6511@126.com>
2021-06-08 20:36:39 +0800

    refactor: 修改字段名

--- src/main/java/top/aengus/panther/enums/NamingRule.java
+++ src/main/java/top/aengus/panther/enums/NamingRule.java
@@ -9,16 +9,16 @@ public enum NamingRule {
     DATE_UUID_UNDERLINE(4, "使用「日期_UUID」作为文件名"),
     DATE_ORIGIN_UNDERLINE(5, "使用「日期_原始名称」作为文件名");
 
-    private final Integer value;
+    private final Integer code;
     private final String desc;
 
-    NamingRule(Integer value, String desc) {
-        this.value = value;
+    NamingRule(Integer code, String desc) {
+        this.code = code;
         this.desc = desc;
     }
 
-    public Integer getValue() {
-        return value;
+    public Integer getCode() {
+        return code;
     }
 
     public String getDesc() {
@@ -27,7 +27,7 @@ public enum NamingRule {
 
     public static NamingRule fromCode(Integer code) {
         for (NamingRule rule : NamingRule.values()) {
-            if (rule.value.equals(code)) {
+            if (rule.code.equals(code)) {
                 return rule;
             }
         }
