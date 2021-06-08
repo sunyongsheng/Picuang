149ddc39fb537f8351cb4e9997086153bcd74083
Aengus Sun <sys6511@126.com>
2021-06-08 21:06:49 +0800

    feat: 修改表结构

--- src/main/java/top/aengus/panther/model/ImageModel.java
+++ src/main/java/top/aengus/panther/model/ImageModel.java
@@ -5,19 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.Table;
 import top.aengus.panther.enums.ImageStatus;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 /**
  * @author Aengus Sun (sys6511@126.com)
  * <p>
  * date 2021/1/19
  */
-@Entity(name = "images")
-@Table(appliesTo = "images", comment = "上传图片记录表")
+@Entity
+@Table(name = "tb_image")
 @DynamicInsert
 @DynamicUpdate
 @Data
@@ -26,32 +24,25 @@ import java.sql.Timestamp;
 public class ImageModel {
 
     @Id
-    @Column(name = "image_id", unique = true, nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long imageId;
+    private Long id;
 
-    @Column(name = "original_name")
+    private String url;
+
     private String originalName;
 
-    @Column(name = "save_name")
     private String saveName;
 
-    @Column(name = "absolute_path")
     private String absolutePath;
 
-    @Column(name = "relative_path")
     private String relativePath;
 
-    @Column(name = "upload_time", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false)
-    private Timestamp uploadTime;
+    private Long uploadTime;
 
-    @Column(name = "creator")
     private String creator;
 
-    @Column(name = "size")
     private Long size;
 
-    @Column(name = "status", columnDefinition = "TINYINT DEFAULT 0 COMMENT '0: 正常，-1: 已删除'")
     private int status;
 
     public ImageModel(String originalName, String saveName, String absolutePath, int status) {
